57134f53daa556fda425cbd93618e301537f0f7b
Nam Phan <phnam181102@gmail.com>
2022-10-05 18:53:55 +0700

    clear

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const carsRouter = require('./cars');
-
-function route(app) {
-    app.use('/api/cars', carsRouter);
-    app.use('/', (req, res) => res.send('API is not available!!!'));
-}
-
-module.exports = route;
